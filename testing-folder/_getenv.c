e842cfc91982855f6e33780eaca5b4fcc7d2a72d
thannie-phan <thannie.phan@gmail.com>
2025-08-22 12:40:42 +1000

    Update and rename free_and_getenv.c to _getenv.c

--- testing-folder/_getenv.c
+++ testing-folder/_getenv.c
@@ -4,26 +4,6 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <sys/types.h>
-/**
- * free_args - free allocated memory for arguments
- * @args: arguments to free
- * Return: Success
- */
-void free_args(char **args)
-{
-	int count;
-
-	if (args == NULL)
-		return;
-
-	count = 0;
-	while (args[count] != NULL)
-	{
-		free(args[count]);
-		count++;
-	}
-	free(args);
-}
 
 /**
  * _getenv - get the environment
