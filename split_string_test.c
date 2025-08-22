cc0a9c3def226d222a8577676e122c77408f51de
thannie-phan <thannie.phan@gmail.com>
2025-08-22 14:42:22 +1000

    Update split_string_test.c for betty

--- split_string_test.c
+++ split_string_test.c
@@ -30,6 +30,29 @@ int count_words(char *str)
 	return (count);
 }
 
+char **mem_words_array(char *str)
+{
+	char *copy_to_count;
+	char **words_array;
+	int count;
+
+	copy_to_count = strdup(str);
+	if (copy_to_count == NULL) /* if strdup failed */
+	{
+		return (NULL);
+	}
+
+	count = count_words(copy_to_count);
+	free(copy_to_count);
+	if (count == 0) /* i.e. only spaces in string */
+	{
+		return (NULL);
+	}
+	/* allocate memory for number of commands and null byte */
+	words_array = malloc(sizeof(char *) * (count + 1));
+		return (words_array);
+}
+
 /**
  * split_string - function to split a string into each word and store
  * the words in a pointer array
@@ -40,27 +63,17 @@ int count_words(char *str)
 char **split_string(char *str)
 {
 	char **words_array; /* array to store pointer to words */
-	char *word, *str_copy, *copy_to_count;
-	int count, slot, free_count;
+	char *word, *str_copy;
+	int slot, free_count;
 
 	slot = 0; free_count = 0;
 
 	if (str == NULL)
 		return (NULL);
-
-	copy_to_count = strdup(str);
-	if (copy_to_count == NULL) /* if strdup failed */
+	words_array = mem_words_array(str);
+	
+	if (words_array == NULL)
 		return (NULL);
-
-	count = count_words(copy_to_count);
-	free(copy_to_count);
-	if (count == 0) /* i.e. only spaces in string */
-		return (NULL);
-	/* allocate memory for number of commands and null byte */
-	words_array = malloc(sizeof(char *) * (count + 1));
-	if (words_array == NULL) /* if malloc fail */
-		return (NULL);
-
 	str_copy = strdup(str);
 	if (str_copy == NULL)
 	{
